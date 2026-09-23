0ea836c66c5ce4d11818a3cfafda9aeb604aee6b
jaskaran-div <jaskaran@derivion.in>
2026-09-23 16:21:33 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Derivion — Inspiring Minds, Igniting Change",
@@ -18,9 +19,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full antialiased scroll-smooth" data-scroll-behavior="smooth">
+    <html
+      lang="en"
+      className="h-full antialiased scroll-smooth"
+      data-scroll-behavior="smooth"
+    >
       <body className="min-h-full flex flex-col overflow-x-hidden bg-white text-[#000000]">
         {children}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S92D0LHS76"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-S92D0LHS76');
+          `}
+        </Script>
       </body>
     </html>
   );
